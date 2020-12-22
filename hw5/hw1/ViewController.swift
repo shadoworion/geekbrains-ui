881fbfd109382f138eb08711d133dbd17d1fc1e7
David Dakhovich <i@wyesoftware.com>
2020-12-22 08:32:48 +0200

    fifth homework

---
+++ hw5/hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by David Dakhovich on 05/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+         view.addGestureRecognizer(tapGesture)
+    }
+}
